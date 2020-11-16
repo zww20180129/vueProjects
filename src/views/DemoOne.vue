dd5beb668e07568ea8123e0aebde4debd2792ccf
季末花落 <1969093335@qq.com>
2020-11-16 12:52:08 +0800

    表单代码

--- src/views/DemoOne.vue
+++ src/views/DemoOne.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="demoOne">
-	  <div>DemoOne页面<div>
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DemoOne',
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: '',
+      },
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    },
+  },
 }
 </script>
